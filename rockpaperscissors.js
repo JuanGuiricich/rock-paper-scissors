cb54ffcd8c6c5a61e9fc767faf726b6e2c314d42
Juan Guiricich <jguiricich@gmail.com>
2023-09-25 21:57:39 -0300

    add all js code for the app to work

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,48 +1,88 @@
+let playerScore = 0;
+let computerScore = 0;
+const btn = document.querySelectorAll('#btn');
+const buttons = document.querySelector('.buttons');
+const result = document.querySelector('#result');
+const player = document.querySelector('#playerScore');
+const computer = document.querySelector('#computerScore');
+const playerChoice = document.querySelector('#playerChoice');
+const computerChoice = document.querySelector('#computerChoice');
+const reset = document.querySelector('#reset');
+
+
 const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
 
-let playerScore = 0;
-let computerScore = 0;
-function game() {
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return ("It's a tie!");
-    } else if (playerSelection === "rock") {
-      if (computerSelection === "paper") {
-        computerScore++;
-        return ("You lose! Paper beats rock.");
-      } else {
-        playerScore++;
-        return ("You win! Rock beats scissors.");
-      }
-    } else if (playerSelection === "paper") {
-      if (computerSelection === "scissors") {
-        computerScore++;
-        return ("You lose! Scissors beats paper.");
-      } else {
-        playerScore++;
-        return ("You win! Paper beats rock.");
-      }
-    } else if (playerSelection === "scissors") {
-      if (computerSelection === "rock") {
-        computerScore++;
-        return ("You lose! Rock beats scissors.");
-      } else {
-        playerScore++;
-        return ("You win! Scissors beats paper.");
-      }
+btn.forEach((button) => {
+  if (playerScore < 5 && computerScore < 5) {
+    button.addEventListener('click', () => {
+      playRound(button.innerText.toLowerCase(), getComputerChoice());
+    })
+  }
+});
+
+function playRound(playerSelection, computerSelection) {
+  playerChoice.innerText = "You chose : " + playerSelection;
+  computerChoice.innerText = "Computer chose : " + computerSelection;
+  if (playerScore >= 5 || computerScore >= 5) {
+    return;
+  }
+
+  if (playerSelection === computerSelection) {
+    result.innerText = "It's a tie!"
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      computerScore++;
+      result.innerText = "You lose! Paper beats rock."
     } else {
-      return ("Invalid choice! Please try again.");
+      playerScore++;
+      result.innerText = "You win! Rock beats scissors."
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      computerScore++;
+      result.innerText = "You lose! Scissors beats paper."
+    } else {
+      playerScore++;
+      result.innerText = "You win! Paper beats rock."
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      computerScore++;
+      result.innerText = "You lose! Rock beats scissors."
+    } else {
+      playerScore++;
+      result.innerText = "You win! Scissors beats paper."
+    }
+  } else {
+    result.innerText = "Invalid choice! Please try again."
+  }
+
+  computer.innerText = "Computer Score : " + computerScore;
+  player.innerText = "Player Score : " + playerScore;
+
+  if (playerScore === 5 || computerScore === 5 ) {
+    buttons.classList.add('disabled');
+    reset.classList.remove('disabled');
+    if (playerScore > computerScore) {
+      result.innerText = "You win! Congratulations!"
+    } else {
+      result.innerText = "You lose! Better luck next time!"
     }
   }
-  while ((playerScore < 5) && (computerScore < 5)) {
-    let playerSelection = prompt("Rock, Paper, or Scissors?").toLocaleLowerCase();
-    computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  console.log("Player Score: " + playerScore);
-  console.log("Computer Score: " + computerScore);
 }
+
+reset.addEventListener('click', () => {
+  reset.classList.add('disabled');
+  playerScore = 0;
+  computerScore = 0;
+  computer.innerText = "Computer Score : " + computerScore;
+  player.innerText = "Player Score : " + playerScore;
+  result.innerText = "Let's play!";
+  playerChoice.innerText = "";
+  computerChoice.innerText = "";
+  buttons.classList.remove('disabled');
+});
